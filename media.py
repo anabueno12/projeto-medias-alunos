7adb45231396d41d6161f36ec4db331ebf4a43af
anabueno12 <analuizabu2716@gmail.com>
2024-11-22 19:42:35 -0300

    APROVADO OU REPROVADO

--- media.py
+++ media.py
@@ -8,5 +8,11 @@ nota3 = float(input("Digite a terceira nota: "))
 # Processo: Calcular a média aritmética
 media = (nota1 + nota2 + nota3) / 3
 
-# Saída: Exibir o resultado
+# Saída: Exibir o resultado da média
 print(f"A média do aluno é: {media:.2f}")
+
+# Verificar se o aluno foi aprovado ou reprovado
+if media >= 6:
+    print("Aprovado")
+else:
+    print("Reprovado")
